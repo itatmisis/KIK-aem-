28f3f5df209619eabd65f23da00ed332085d06ed
algakz06 <77662392+algakz06@users.noreply.github.com>
2022-11-02 17:01:20 +0300

    added queries

--- server/query.py
+++ server/query.py
@@ -1,3 +1,5 @@
 insert_user = "INSERT INTO users (login, password, is_admin, is_super) VALUES ('%s', '%s', %s, %s)"
-
+select_salt = "SELECT salt FROM users WHERE login = '%s'"
+select_user = "SELECT COUNT() FROM users WHERE login = '%s' and password = '%s'"
+get_pass = "SELECT password FROM users WHERE login = %s"
 
